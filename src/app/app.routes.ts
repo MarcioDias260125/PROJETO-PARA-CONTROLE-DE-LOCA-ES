a3316e01fb9661adac39cc2e4b9e8a70ad639684
Marcio Dias <marcio.d.lima6@aluno.senai.br>
2026-08-16 16:43:58 -0300

    Protótipo copiado do Claude

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { FornecedorComponent } from './component/fornecedor-component/fornecedor-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'fornecedores',
+        component: FornecedorComponent
+    }
+];
