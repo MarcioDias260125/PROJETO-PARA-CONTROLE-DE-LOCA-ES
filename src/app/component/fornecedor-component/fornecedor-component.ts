0998fe7d9c43a17547f84492eb2dacfa25329c1f
Marcio Dias <marcio.d.lima6@aluno.senai.br>
2026-08-16 15:55:22 -0300

    Criado o componente onde irei armazenar os fornecedores

---
+++ src/app/component/fornecedor-component/fornecedor-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fornecedor-component',
+  imports: [],
+  templateUrl: './fornecedor-component.html',
+  styleUrl: './fornecedor-component.css',
+})
+export class FornecedorComponent {}
